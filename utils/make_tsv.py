9448f8abcc32af64886f13bfef758489f7600761
c <ckdeutschbein@willamette.edu>
2024-06-07 19:40:25 -0700

    make_tsv upgrade

--- utils/make_tsv.py
+++ utils/make_tsv.py
@@ -4,8 +4,6 @@ import sys, os, hashlib, base64
 
 PREFIX = 'https://raw.githubusercontent.com/cd-public/books/main/' # DONT scrap Gutenberg
 MAX_FS = 10 if len(sys.argv) == 1 else int(sys.argv[1])
+BK_DIR = '../books/'
 
-tsv = open("books.tsv",'w')
-
-os.chdir("../books")
-tsv.writelines(['TsvHttpData-1.0\n'] + ['%s%s\t%s\t%s\n' % (PREFIX, f, os.path.getsize(f), base64.b64encode(hashlib.md5(open(f,'rb').read()).digest()).decode("utf-8")) for f in os.listdir()[:MAX_FS] if '.txt' in f])
+open("books.tsv",'w').writelines(['TsvHttpData-1.0\n'] + ['%s%s\t%s\t%s\n' % (PREFIX, f, os.path.getsize(BK_DIR + f), base64.b64encode(hashlib.md5(open(BK_DIR + f,'rb').read()).digest()).decode("utf-8")) for f in os.listdir(BK_DIR)[:MAX_FS] if '.txt' in f])
